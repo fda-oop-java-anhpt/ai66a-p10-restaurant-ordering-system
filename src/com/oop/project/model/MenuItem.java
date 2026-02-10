348d189ec3e4065ae3678ddf10d510e7def3db35
hoang3003 <minhhoangtran3003@gmail.com>
2026-02-10 23:09:55 +0700

    add menu item

---
+++ src/com/oop/project/model/MenuItem.java
@@ -0,0 +1,43 @@
+package com.oop.project.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class MenuItem {
+    private int id;
+    private String name;
+    private String description;
+    private BigDecimal basePrice;
+    private int categoryId;
+    private LocalDateTime createdAt;
+
+    public MenuItem(
+        int id,
+        String name,
+        String description,
+        BigDecimal basePrice,
+        int categoryId,
+        LocalDateTime createdAt
+    )  {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.basePrice = basePrice;
+        this.categoryId = categoryId;
+        this.createdAt = createdAt;
+    }
+
+    // getters
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public BigDecimal getBasePrice() { return  basePrice; }
+    public int getCategoryId() { return categoryId; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+
+    //setters
+    public void setBasePrice(BigDecimal basePrice) {
+        this.basePrice = basePrice;
+    }
+
+}
