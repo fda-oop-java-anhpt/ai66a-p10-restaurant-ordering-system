f75e499949e5b6c5df7f77cc3f6eeb3d4c4af134
QuangMinhQu <tepbucon@gmail.com>
2026-02-11 09:37:14 +0700

    Update Main UI for Menu features

--- src/com/oop/project/ui/MainFrame.java
+++ src/com/oop/project/ui/MainFrame.java
@@ -13,23 +13,34 @@ import com.oop.project.ui.panels.OrdersPanel;
 
 
 public class MainFrame extends JFrame {
+
+    private final User currentUser;
+
     public MainFrame(User user) {
-        setTitle("Restaurant POS" + user.getUsername());
+        this.currentUser = user;
+        setTitle("Restaurant POS - " + user.getUsername());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1000, 700);
         setLocationRelativeTo(null);
 
         JTabbedPane tabs = new JTabbedPane();
-        tabs.addTab("Menu", new MenuPanel());
-        tabs.addTab("Cart", new CartPanel());
-        tabs.addTab("Orders", new OrdersPanel());
-        tabs.addTab("Dashboard", new DashboardPanel());
-
+        tabs.addTab("Menu", new MenuPanel(currentUser));
+        tabs.addTab("Cart", new CartPanel(/*currentUser */));
+        tabs.addTab("Orders", new OrdersPanel(/*currentUser */));
+        if (currentUser.isManager()) {
+            tabs.addTab("Dashboard", new DashboardPanel(/*currentUser */));
+        }
         setLayout(new BorderLayout());
         add(tabs, BorderLayout.CENTER);
 
+        // Defensive: if Dashboard is ever added elsewhere, remove it for non-managers.
         if (!user.isManager()) {
-            tabs.removeTabAt(3);
+            for (int i = 0; i < tabs.getTabCount(); i++) {
+                if ("Dashboard".equalsIgnoreCase(tabs.getTitleAt(i))) {
+                    tabs.removeTabAt(i);
+                    break;
+                }
+            }
         }
     }
 }
