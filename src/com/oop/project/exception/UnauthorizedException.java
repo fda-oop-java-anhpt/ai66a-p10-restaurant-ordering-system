bd9ed14be2cf3fe1ed5fd271e27ab093d9edb1f4
pen <nguyennguyenkhoi902@gmail/com>
2026-04-04 22:26:40 +0700

    Added an exception handler for invalid logins

--- src/com/oop/project/exception/UnauthorizedException.java
+++ src/com/oop/project/exception/UnauthorizedException.java
@@ -1,9 +1,5 @@
 package com.oop.project.exception;
 
-/**
- * Thrown when a user tries to access a resource
- * without sufficient permission.
- */
 public class UnauthorizedException extends RuntimeException {
 
     public UnauthorizedException() {
