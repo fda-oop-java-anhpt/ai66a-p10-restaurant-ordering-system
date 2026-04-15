dc669d9f8fa56ba2bb3408fea7a9b724f18b5d25
hoang3003 <minhhoangtran3003@gmail.com>
2026-04-15 13:41:47 +0700

    Implement password visibility toggle in LoginFrame and enhance password input layout

--- src/com/oop/project/ui/LoginFrame.java
+++ src/com/oop/project/ui/LoginFrame.java
@@ -1,6 +1,15 @@
 package com.oop.project.ui;
 
+import java.awt.BasicStroke;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
 import javax.swing.BoxLayout;
+import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -16,6 +25,10 @@ public class LoginFrame extends JFrame{
 
     private JTextField txtUsername;
     private JPasswordField txtPassword;
+    private char defaultEchoChar;
+    private boolean passwordVisible;
+    private final Icon eyeOpenIcon = new EyeIcon(false);
+    private final Icon eyeClosedIcon = new EyeIcon(true);
     private final AuthService authService = new AuthService();
 
     public LoginFrame() {
@@ -29,6 +42,20 @@ public class LoginFrame extends JFrame{
 
         txtUsername = new JTextField();
         txtPassword = new JPasswordField();
+        defaultEchoChar = txtPassword.getEchoChar();
+        passwordVisible = false;
+
+        JButton btnTogglePassword = new JButton(eyeClosedIcon);
+        btnTogglePassword.setPreferredSize(new Dimension(34, 24));
+        btnTogglePassword.setMinimumSize(new Dimension(34, 24));
+        btnTogglePassword.setMaximumSize(new Dimension(34, 24));
+        btnTogglePassword.setFocusable(false);
+        btnTogglePassword.setToolTipText("Show password");
+        btnTogglePassword.addActionListener(e -> togglePasswordVisibility(btnTogglePassword));
+
+        JPanel passwordPanel = new JPanel(new BorderLayout(5, 0));
+        passwordPanel.add(txtPassword, BorderLayout.CENTER);
+        passwordPanel.add(btnTogglePassword, BorderLayout.EAST);
         
         JButton btnlogin = new JButton("Login");
         
@@ -37,7 +64,7 @@ public class LoginFrame extends JFrame{
         panel.add(new JLabel("Username"));
         panel.add(txtUsername);
         panel.add(new JLabel("Password"));
-        panel.add(txtPassword);
+        panel.add(passwordPanel);
         panel.add(btnlogin);
 
         add(panel);
@@ -61,5 +88,60 @@ public class LoginFrame extends JFrame{
 
         dispose();
         new MainFrame(user).setVisible(true);
+    }
+
+    private void togglePasswordVisibility(JButton btnTogglePassword) {
+        passwordVisible = !passwordVisible;
+        if (passwordVisible) {
+            txtPassword.setEchoChar((char) 0);
+            btnTogglePassword.setToolTipText("Hide password");
+            btnTogglePassword.setIcon(eyeOpenIcon);
+            return;
+        }
+
+        txtPassword.setEchoChar(defaultEchoChar);
+        btnTogglePassword.setToolTipText("Show password");
+        btnTogglePassword.setIcon(eyeClosedIcon);
+    }
+
+    private static class EyeIcon implements Icon {
+        private final boolean crossed;
+
+        private EyeIcon(boolean crossed) {
+            this.crossed = crossed;
+        }
+
+        @Override
+        public int getIconWidth() {
+            return 14;
+        }
+
+        @Override
+        public int getIconHeight() {
+            return 14;
+        }
+
+        @Override
+        public void paintIcon(java.awt.Component c, Graphics g, int x, int y) {
+            Graphics2D g2 = (Graphics2D) g.create();
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2.setColor(Color.DARK_GRAY);
+            g2.setStroke(new BasicStroke(1.6f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+
+            int left = x + 1;
+            int top = y + 2;
+            int width = 12;
+            int height = 9;
+
+            g2.drawArc(left, top, width, height, 0, 180);
+            g2.drawArc(left, top, width, height, 0, -180);
+            g2.fillOval(x + 5, y + 5, 4, 4);
+
+            if (crossed) {
+                g2.drawLine(x + 2, y + 12, x + 12, y + 2);
+            }
+
+            g2.dispose();
+        }
     }
 }
